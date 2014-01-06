b4c21985f544824b1572a4b3e3d40b3abca0d804
edamron <earl@earldamron.com>
2014-01-06 04:06:38 -0800

    added my app to consume the API, and have made several changes to make it work like a basic currency conversion on the Google search results

--- UI/CurrencyConverter/Models/ExchangeInfo.cs
+++ UI/CurrencyConverter/Models/ExchangeInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Net;
+using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace CurrencyConverter.Models {
     public class CountryExchangeInfo {
@@ -10,8 +10,44 @@ namespace CurrencyConverter.Models {
         public double ExchangeRate { get; set; }
     }
 
-    public class CurrencyConversion {
-        public CountryExchangeInfo SourceCountry { get; set; }
-        public CountryExchangeInfo TargetCountry { get; set; }
+    public class WorldCurrencies {
+        public IEnumerable<CountryExchangeInfo> Currencies { get; set; }
+        public Dictionary<string, double> TargetRates { get; set; }
+
+        public WorldCurrencies() {
+            string url = "http://earl.azurewebsites.net/api/currencyexchange";
+            WebRequest request = WebRequest.Create(url);
+            WebResponse ws = request.GetResponse();
+
+            System.IO.StreamReader sr = new System.IO.StreamReader(ws.GetResponseStream());
+            string json = sr.ReadToEnd();
+
+            Currencies = JsonConvert.DeserializeObject<IEnumerable<CountryExchangeInfo>>(json);
+
+            // this still needs to be dynamic
+            // (by using the rates as is for the US Dollar,
+            //  or changing them all if not the US Dollar)
+            TargetRates = new Dictionary<string, double>();
+
+            foreach (var countryExchangeInfo in Currencies) {
+                TargetRates.Add(countryExchangeInfo.Code, countryExchangeInfo.ExchangeRate);
+            }
+        }
+
+        public WorldCurrencies(string SourceCurrency) : this() {
+            // adjust the exchange rates to be based on the selected source currency
+            double newSourceRate = 1;
+            TargetRates.TryGetValue(SourceCurrency, out newSourceRate);
+
+            TargetRates.Clear();
+
+            foreach (var countryExchangeInfo in Currencies) {
+                TargetRates.Add(countryExchangeInfo.Code, countryExchangeInfo.ExchangeRate / newSourceRate);
+            }
+        }
+    }
+
+    public class ConversionRates {
+        public Dictionary<string, double> DestinationRates { get; set; }
     }
 }
